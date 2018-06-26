02ca2444f5a5227e5268857414ff036f69577fab
Fabien MARTY <fabien.marty@gmail.com>
2018-06-26 09:13:35 +0200

    a few modification to get the lib running

--- README.md
+++ README.md
@@ -1,77 +1,29 @@
-lua-resty-statsd
-================
+# lua-resty-statsd
 
-Statsd client for [OpenResty](https://openresty.org/en/) that supports the
-extended/[datadog](http://docs.datadoghq.com/guides/dogstatsd/) protocol.
+## Status
 
-Status
-======
-This library is considered production ready.
+Experimental
 
-Usage
-=====
+## What is it ?
 
-Example openresty/nginx config usage.
+This is a fork of [lua-resty-stats](http://github.com/bakins/lua-resty-statsd).
+
+The referenced project does not work at all for us but is built on good
+ideas. We plan to maintain this fork with some modifications for our use case.
+
+But we are waiting for [license clarification](https://github.com/bakins/lua-resty-statsd/issues/2)
+for the moment.
+
+## Usage
+
 ```
 init_worker_by_lua_block {
-  statsd = require("resty.statsd").new({namespace="nginx"})
+  statsd = require("resty.statsd").new({host="127.0.0.1", port=12345, delay=100})
 }
 
 server {
   log_by_lua_block{
-    statsd.count("requests", 1, 1.0, {someLabel="someValue"})
+    statsd.count(statsd, "requests", 1, 1, {tag1="foo1", tag2="foo2"})
   }
 }
 ```
-
-Methods
-=======
-
-new
----
-
-`statsd = require("resty.statsd").new(options)`
-
-Create a new statsd client. You should create one of these and [reuse](https://github.com/openresty/lua-nginx-module#data-sharing-within-an-nginx-worker) it. The client uses a simple queue internally and pushes stats periodically.
-
-An optional Lua table can be specified:
-* `host` - statsd host. default: _127.0.0.1_
-* `port` - statsd port. default: _8125_
-* `namespace` - this will be prefixed to every metric emitted. A namespace of "foo" and a metric name of "bar" would emit a metric named "foo.bar". There is no default.
-* `tags` -  a table of key=value lables that will be added to every metric.
-* `timeout` - socket timeout in miliseconds for send.default: _250_
-* `delay` - how often to send flush the metrics queue in miliseconds. default: _250_
-
-
-gauge
------
-
-`statsd:gauge(key, value, sample_rate, tags)`
-
-Set a gauge value. Value should be an integer.  
-
-If sample_rate is nil, it defaults to 1.0 - ie, no sampling.
-
-Tags is a table of key=value that will be merged with those passed to `new` - if any.
-
-count
------
-
-`statsd:count(key, value, sample_rate, tags)`
-
-Increment a counter by value. To decrement,  use a negative integer.
-
-If sample_rate is nil, it defaults to 1.0 - ie, no sampling.
-
-Tags is a table of key=value that will be merged with those passed to `new` - if any.
-
-timing
------
-
-`statsd:timing(key, value, sample_rate, tags)`
-
-Record a timing. Value should be an integer in milliseconds.
-
-If sample_rate is nil, it defaults to 1.0 - ie, no sampling.
-
-Tags is a table of key=value that will be merged with those passed to `new` - if any.
--- lib/resty/statsd.lua
+++ lib/resty/statsd.lua
@@ -9,7 +9,6 @@ function _M.new(options)
     local self = {
         host = options.host or "127.0.0.1",
         port = options.port or 8125,
-        namespace = options.namespace,
         tags = options.tags or {},
         timeout = options.timeout or 250,
         queue = {},
@@ -19,38 +18,41 @@ function _M.new(options)
     return setmetatable(self, mt)
 end
 
-local concat = table.concat
-
 local function create_message(self, key, value, kind, sample_rate, tags)
     local rate = ""
     if sample_rate and sample_rate ~= 1 then
         rate = "|@"..sample_rate
     end
 
-    local tag_string = ""
-    local self_tags = self.tags
-    if tags and self_tags then
-        tag_string = "|#" .. self_tags .. "," .. tags
-    else
-        if self_tags then
-            tag_string = "|#" .. self_tags
-        else
-            if tags then
-                tag_string = "|#" .. tags
-            end
+    local new_table = {}
+    local k = nil
+    local v = nil
+    if self.tags ~= nil then
+        for k, v in pairs(self.tags) do
+            table.insert(new_table, ",")
+            table.insert(new_table, k)
+            table.insert(new_table, "=")
+            table.insert(new_table, v)
         end
     end
+    if tags ~= nil then
+        for k, v in pairs(tags) do
+            table.insert(new_table, ",")
+            table.insert(new_table, k)
+            table.insert(new_table, "=")
+            table.insert(new_table, value)
+        end
+    end
+    local tag_string = table.concat(new_table)
 
     local message = {
-        self.namespace,
-        ".",
         key,
+        tag_string,
         ":",
         value,
         "|",
         kind,
         rate,
-        tag_string,
         "\n"
     }
     return message
@@ -65,7 +67,6 @@ local function set_timer(self)
     end
 end
 
-local remove = table.remove
 send_messages = function(premature, self)
     if premature then
         return
@@ -86,10 +87,12 @@ send_messages = function(premature, self)
 
     local q = self.queue
     for i=1,#q do
-        local message = q[i]
-        local ok, err = sock:send(message)
-        if not ok then
-            ngx.log(ngx.ERR, "send failed for ", self.host, ":", self.port, " => ", err)
+        if q[i] ~= nil then
+            local message = table.concat(q[i])
+            local ok, err = sock:send(message)
+            if not ok then
+                ngx.log(ngx.ERR, "send failed for ", self.host, ":", self.port, " => ", err)
+            end
         end
     end
     sock:close()
@@ -110,6 +113,10 @@ end
 
 function _M.count(self, key, value, sample_rate, tags)
     return queue(self, key, value, "c", sample_rate, tags)
+end
+
+function _M.set(self, key, value, tags)
+    return queue(self, key, value, "s", sample_rate, tags)
 end
 
 function _M.timing(self, key, value, tags)
