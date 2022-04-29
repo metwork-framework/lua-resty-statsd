c5a7dac37f6eb4dd8e780f8e4abf2e896eea247d
thebaptiste <jean-baptiste.veslin@meteo.fr>
2022-04-29 15:55:50 +0000

    build: move CI to github actions

---
+++ .github/workflows/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -x
+
+yum -y install wget make
+wget -O /usr/local/bin/lua-releng "https://raw.githubusercontent.com/openresty/openresty-devel-utils/master/lua-releng"
+chmod +x /usr/local/bin/lua-releng
+make test
