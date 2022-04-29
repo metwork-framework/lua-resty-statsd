4f44b42f31f7d0cd682ab050f4be6cb5a87799b6
thebaptiste <jean-baptiste.veslin@meteo.fr>
2022-04-29 15:59:52 +0000

    build: fix CI : add find

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-yum -y install wget make
+yum -y install wget make findutils
 wget -O /usr/local/bin/lua-releng "https://raw.githubusercontent.com/openresty/openresty-devel-utils/master/lua-releng"
 chmod +x /usr/local/bin/lua-releng
 cd /src
