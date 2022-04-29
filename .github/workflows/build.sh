0c36543577548353eea1c0b3ba420f64d58e2796
thebaptiste <jean-baptiste.veslin@meteo.fr>
2022-04-29 15:57:05 +0000

    build: fix CI

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -5,4 +5,5 @@ set -x
 yum -y install wget make
 wget -O /usr/local/bin/lua-releng "https://raw.githubusercontent.com/openresty/openresty-devel-utils/master/lua-releng"
 chmod +x /usr/local/bin/lua-releng
+cd /src
 make test
